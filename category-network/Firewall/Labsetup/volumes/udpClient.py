e8d3c90179836cf7663420c8d97cb37c2698395c
Tom Kelliher <kelliher@goucher.edu>
2024-04-21 17:30:45 -0400

    Add test client program for Task 4, round robin.

---
+++ category-network/Firewall/Labsetup/volumes/udpClient.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# Send 'count' packets to the host with ip address 'ip'
+# using port 'port'.
+
+import socket
+
+count = 12
+ip = "10.9.0.11"
+port = 8080
+
+for i in range(count):
+    print('Sending packet ' + str(i))
+    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    skt.sendto(str.encode('packet ' + str(i) + '\n'), (ip, port))
+    skt.close()
