8d4e7d3e0ddafd5fd012699c3a992b67f441c0d8
Tom Kelliher <kelliher@goucher.edu>
2024-04-21 22:49:34 -0400

    Provide a more useful UDP server for Task 4.

---
+++ category-network/Firewall/Labsetup/volumes/udpServer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Receive UDP packets each of max size 'max_rcv' on any interface, listening
+# on port 'port'.  Display a received packets count and the contents of
+# each received packet.
+#
+# Terminate the server using ctrl-c.
+
+import socket
+
+ip = ''
+port = 8080
+max_rcv = 1024
+cnt = 0
+
+skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+skt.bind((ip, port))
+
+while True:
+    data = skt.recv(max_rcv)
+    print('Packet ' + str(cnt) + ': ' + data.decode('utf-8'), end='')
+    cnt += 1
